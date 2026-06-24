8bec8396f1e173444c28236fb4823568d436a24d
Affan al <affan2k05@gmail.com>
2026-06-24 17:43:07 +0500

    style: remove extraneous whitespace from route configuration export

--- first-vibecoding-app/web/routes.ts
+++ first-vibecoding-app/web/routes.ts
@@ -1,4 +1,4 @@
 import { flatRoutes } from "@react-router/fs-routes";
-import { type RouteConfig } from "@react-router/dev/routes"; 
+import { type RouteConfig } from "@react-router/dev/routes";
 
-export default flatRoutes() satisfies RouteConfig ;
+export default flatRoutes() satisfies RouteConfig;
